189ab2b65d7fdf4e53f3eb0b6602dfccbe8dc6ff
czhang <222222>
2017-03-07 10:52:02 +0800

    添加

--- app/src/main/java/com/gwtravel/view/activity/MainActivity.java
+++ app/src/main/java/com/gwtravel/view/activity/MainActivity.java
@@ -1,12 +1,25 @@
 package com.gwtravel.view.activity;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.gwtravel.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
+
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.iv_user)
+    ImageView iv_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +28,19 @@ public class MainActivity extends BaseActivity {
         ButterKnife.bind(this);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    @OnClick({R.id.iv_user,R.id.iv_message})
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.iv_user:
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+                break;
+            case R.id.iv_message:
+
+                break;
+
+        }
     }
 
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
 }
